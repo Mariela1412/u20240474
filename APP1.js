edd824a3684d26c0811046fca3be9a2471be2bb4
SevagothTeDarkFate <140095731+SevagothTeDarkFate@users.noreply.github.com>
2024-07-29 10:25:09 -0600

    Mariela

---
+++ APP1.js
@@ -0,0 +1,10 @@
+let readline = require('readline')
+
+let rl= readline.createInterface(
+    process.stdin,
+    process.stdout
+)
+
+rl.question('Cual es su nombre? ',(nombre)=>{
+    console.log('Hola, '+ nombre + '!')
+ })
